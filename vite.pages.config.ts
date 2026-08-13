d67a965fe748d90592116957a335b8273566b7a9
soo7894 <iridescent2025abc@gmail.com>
2026-08-13 13:14:08 +0900

    Add vite.pages.config.ts

---
+++ vite.pages.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "pages",
+  base: "/harusallim/",
+  publicDir: "../public",
+  plugins: [react()],
+  build: {
+    outDir: "../pages-dist",
+    emptyOutDir: true,
+  },
+});
+
